cb1a03dbec17a826e8fc06d79a2f329d381e1a6a
BoyKaYK <88595925+BoyKaYK@users.noreply.github.com>
2022-08-15 14:26:02 +0200

    steam_logger v1.4 update
    Added new funtion to help "translating" selenium cookie file to "requests"

--- steam_logger.py
+++ steam_logger.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.common.by import By
 from data import *
-Link = 'https://store.steampowered.com/login'
+Link = 'https://store.steampowered.com'
 Cookie_link = 'https://steamcommunity.com/'
 class Steam_logger:
     def __init__(user):
@@ -26,7 +26,7 @@ class Steam_logger:
 
     def steam_login(user) -> None:
         driver = user.driver
-        driver.get(Link)
+        driver.get(Cookie_link)
         driver.maximize_window()
         driver.implicitly_wait(5)
 
@@ -39,7 +39,7 @@ class Steam_logger:
         else:
             driver.find_element(By.CLASS_NAME, 'global_action_link').click()
             sleep(1)
-            
+
             driver.find_element(By.NAME, "username").send_keys(USERNAME)
             sleep(2)
 
@@ -69,11 +69,22 @@ class Steam_logger:
             sleep(2)
             user.driver.find_element(By.CSS_SELECTOR, "div[type=submit]").click()
             sleep(2)
+
+    def get_selenium_cookie(user):
+        cookies = {}
+        if os.path.exists("cookie.pieckle"):
+            selenium_cookies = user.driver.get_cookies() 
+            with open("cookie.pieckle", 'rb') as file:
+                selenium_cookies = pickle.load(file)
+            return selenium_cookies
+        else:
+            return "no cookie"
         
 
 
 #bot_Steam = Steam_logger()
 #bot_Steam.steam_login()
+
 
 
        
